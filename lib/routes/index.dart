9c80ae9cfe232a3c6a74e403ff21424d38871d63
Adx <2243623014@qq.com>
2025-12-22 09:50:14 +0800

    搭建基础路由和组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,21 @@
+//  管理路由
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/login.dart';
+import 'package:hm_shop/pages/main.dart';
+
+//  返回app根组件
+Widget getRootWidget() {
+  return MaterialApp(
+    //  命名路由
+    initialRoute: "/",
+    routes: getRootRoutes(),
+  );
+}
+
+//  返回路由配置
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {
+    "/" : (context) => MainPage(),
+    "/login" : (context) => LoginPage()
+  };
+}
